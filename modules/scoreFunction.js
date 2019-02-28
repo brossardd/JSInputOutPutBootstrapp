555fcb068c584e8bb830a1aa581f85e92b1eae2f
Anthony Pena <anthony.pena@outlook.fr>
2019-02-28 19:27:27 +0100

    score

--- modules/scoreFunction.js
+++ modules/scoreFunction.js
@@ -1,4 +1,28 @@
-'use strict'
+"use strict";
+
+function extractTags({photos}) {
+  let tags = {};
+  for(const p of photos) {
+    for(const t of p.tags) {
+      tags[t] = t;
+    }
+  }
+  return Object.keys(tags);
+}
+
+function intersecTags(tags1, tags2) {
+  const inter = [];
+  for(const t of tags1) {
+    if(tags2.indexOf(t) !== -1) {
+      inter.push(t);
+    }
+  }
+  return inter;
+}
+
+function diffTags(base, minus) {
+  return base.filter(t => minus.indexOf(t) === -1);
+}
 
 /**
  *
@@ -9,14 +33,22 @@
  * @param {Object} output
  * @returns {number}
  */
-function computeScore(input, output){
-    let score = 0;
+function computeScore(input, output) {
+  let score = 0;
+  const slides = output.slides;
+  const nbIntersec = slides.length - 1;
 
-    /**
-     * Code Goes Here ▼
-     */
+  for (let i = 0; i < nbIntersec; i++) {
+    const prevSlideTags = extractTags(slides[i]);
+    const nextSlideTags = extractTags(slides[i+1]);
+    const commonTags = intersecTags(prevSlideTags, nextSlideTags);
+    const uniqTagsPrevSlide = diffTags(prevSlideTags, commonTags);
+    const uniqTagsNextSlide = diffTags(nextSlideTags, commonTags);
+    const minTagNb = Math.min(commonTags.length, uniqTagsPrevSlide.length, uniqTagsNextSlide.length);
+    score += minTagsNb;
+  }
 
-    return score;
+  return score;
 }
 
 module.exports = computeScore;
